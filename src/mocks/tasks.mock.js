b31f8e54b1da3c2a5b84849164d9e677c3176b7e
Franco Aguilera <flaalf@gmail.com>
2022-10-03 16:01:09 -0300

    initial ts commit

--- src/mocks/tasks.mock.js
+++ src/mocks/tasks.mock.js
@@ -1,17 +1,17 @@
 export const tasks = [
   {
     id: "63378c4483be1737c6a881ea",
-    title: "nostrud nulla et",
-    description: "laboris cupidatat ut nostrud proident in ea quis",
+    title: "Title One",
+    description: "Description One",
   },
   {
     id: "63378c44dfb4aa375350b890",
-    title: "adipisicing ad non",
-    description: "aliquip ullamco nisi deserunt eu mollit aliquip quis",
+    title: "Title Two",
+    description: "Description Two",
   },
   {
     id: "63378c44c6603ea97d50e13d",
-    title: "adipisicing irure esse",
-    description: "non laborum eu officia ad sunt ea ut",
+    title: "Title Three",
+    description: "Description Three",
   },
 ];
